742529f9263d2d0920f52129a1ec00198c2f8fc6
kseroff <112875767+kseroff@users.noreply.github.com>
2022-09-12 10:58:10 +0300

    Add files via upload

---
+++ Lab.progr #1/lab1/lab1/lab1.cpp
@@ -0,0 +1,57 @@
+﻿
+#include <iostream>
+#include <time.h>
+#include <iomanip>
+using namespace std;
+
+int main(void)
+{
+	srand(time(NULL));
+	setlocale(0, "");
+
+	int n, m, max = 0, min = 999;
+	cout << "Введите размер матрицы: ";
+	cin >> n >> m;
+	int** arr = new int*[n];
+	for(int i=0; i<n; i++)
+	{
+		arr[i] = new int[m];
+	}
+
+	cout << "Массив: " << endl;
+	for (int i = 0; i < n; i++)
+	{
+		int sum = 0;
+		cout <<endl;
+		for (int j = 0; j < m; j++) 
+		{
+			arr[i][j] = -10 + rand() % 51;
+
+			if (arr[i][j] > max) { max = arr[i][j]; }
+			if (arr[i][j] < min) { min = arr[i][j]; }
+			sum += arr[i][j];
+			cout <<setw(3)<< arr[i][j] << " ";
+		}
+		cout << "--- сумма: " << sum;
+	}
+	cout << endl << endl;
+	for (int i = 0; i < n; i++)
+	{
+		int sum1 = 0;
+		for (int j = 0; j < m; j++)
+		{
+			sum1 += arr[j][i];
+		}
+		cout << "Столбец #" << i+1 << " Сумма: " << sum1<<endl;
+	}
+
+	cout<<endl<<endl<<"Разница максимального и минимального элемента: " << max - min;
+
+	for (int i = 0; i < n; i++)
+		delete[] arr[i];
+	delete[] arr;
+
+	cin.get(); cin.get();
+	return 0;
+}
+
